1c8adb4b56dc1ff42fffce11e1a5263e107b8061
agiratech-praveenm <praveen.m@agiratech.com>
2024-10-14 17:48:55 +0530

    push_03: bring task under PrimaryLayout

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import TaskManagement from "../pages/task";
+import PrimaryLayout from "../layout/PrimaryLayout";
+import Document from "../pages/documents";
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<TaskManagement />} />
+        <Route path="/" element={<PrimaryLayout/>} >
+            <Route path="/task" element={<TaskManagement/>} />
+            <Route path="/documents" element={<Document/>} />
+        </Route>
       </Routes>
     </Router>
   );
