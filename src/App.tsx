068984bd3809e2751582ce3f170914529404c981
agiratech-praveenm <praveen.m@agiratech.com>
2024-10-16 18:33:13 +0530

    push_02: navbar reduced css, added ConfigProvider in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
-import './App.css'
-import AppRoutes from './routes'
+// import './App.css';
+import { ConfigProvider } from 'antd';
+import AppRoutes from './routes';
+import 'antd/dist/reset.css'; // Reset Ant Design styles
+import { customTheme } from './theme';
 
 function App() {
 
   return (
-    <AppRoutes/>
+    <ConfigProvider theme={customTheme}>
+      <AppRoutes/>
+    </ConfigProvider>
+    
   )
 }
 
-export default App
+export default App;
+
+// import React from 'react';
+// import { ConfigProvider } from 'antd';
+// import AppRoutes from './App';
+// import 'antd/dist/reset.css'; // Reset Ant Design styles
+
+
+// ReactDOM.render(
+//   // <React.StrictMode>
+//     <ConfigProvider>
+//       <App />
+//     </ConfigProvider>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+// theme.ts
+export const customTheme = {
+    token: {
+      colorPrimary: '#00498F', // Primary color
+      colorText: '#36454F', // Text color
+      colorBgLayout: '#ffffff', // Layout background color
+      colorBorder: '#d9d9d9', // Border color
+      borderRadius: 8, // Global border radius
+      fontSize: 16, // Base font size
+      
+    },
+  };
+  
